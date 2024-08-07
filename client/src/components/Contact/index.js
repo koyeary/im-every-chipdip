0c1f49cf4351de4accbf3b2f7e349c5b11d6b92a
Kat Yeary <katyeary@gmail.com>
2024-08-07 18:55:34 -0400

    contact form complete with messaging

--- client/src/components/Contact/index.js
+++ client/src/components/Contact/index.js
@@ -1,12 +1,71 @@
+import { useState } from "react";
+import Alert from "@mui/material/Alert";
+import AlertTitle from "@mui/material/AlertTitle";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
+import emailjs from "@emailjs/browser";
+import { sendEmail } from "../../actions/Contact";
 import "./Contact.css";
 
 const Contact = ({ darkMode }) => {
-  // Your component logic here
+  const [show, setShow] = useState(false);
+  const [severity, setSeverity] = useState("");
+  const [msg, setMsg] = useState("");
+  const [emailData, setEmailData] = useState({
+    name: "",
+    email: "",
+    subject: "",
+    message: "",
+  });
+  const { name, email, subject, message } = emailData;
+
+  const handleChange = (e) => {
+    e.preventDefault();
+
+    setEmailData({ ...emailData, [e.target.name]: e.target.value });
+  };
+
+  const sendToast = (msg, severity) => {
+    setSeverity(severity);
+    setMsg(msg);
+  };
+
+  const handleClear = () => {
+    setEmailData({
+      name: "",
+      email: "",
+      subject: "",
+      message: "",
+    });
+  };
+
+  const handleSend = (e) => {
+    e.preventDefault();
+    sendEmail(emailData, sendToast, handleClear);
+    /*  emailjs
+      .send(
+        "default_service",
+        "template_Rq3s9o8b",
+        emailData,
+        "user_G4m0Vhk6hpxUONewRQh00",
+       
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          sendToast(result.text, "success");
+          handleClear();
+        },
+        (error) => {
+          console.log(error.text);
+          sendToast(error.text, "error");
+        }
+      ); */
+  };
 
   return (
-    <div
+    <form
+      onSubmit={handleSend}
       style={{
         width: "90%",
         maxWidth: 800,
@@ -20,20 +79,66 @@ const Contact = ({ darkMode }) => {
         justifyContent: "center",
       }}
     >
-      <div className="header-wrap">
-        <h1 className="contact-header">Send me a message.</h1>
-        <h2 className="subheader">
-          Or contact me directly at katyeary @ gmail dot com
-        </h2>
-      </div>
-      <TextField id="outlined-basic" label="Your Name" />
-      <TextField id="outlined-basic" label="Your Email" />
-      <TextField id="outlined-basic" label="Subject" />
-      <TextField id="outlined-basic" label="Message" multiline rows={10} />
-      <Button style={{ width: 200, margin: "0 auto" }} variant="contained">
-        Send
-      </Button>
-    </div>
+      {msg === "" ? (
+        <div className="header-wrap">
+          <h1 className="contact-header">Send me a message.</h1>
+          <h2 className="subheader">
+            Or contact me directly at katyeary @ gmail dot com
+          </h2>
+        </div>
+      ) : (
+        <>
+          <div className="header-wrap">
+            <h1 className="contact-header">Send me a message.</h1>
+          </div>
+          <Alert severity={severity} style={{ zIndex: 1000 }}>
+            <AlertTitle>
+              {severity.charAt(0).toUpperCase() + severity.slice(1)}
+            </AlertTitle>
+            {msg}
+          </Alert>
+        </>
+      )}
+      <>
+        <TextField
+          onChange={handleChange}
+          name="name"
+          id="outlined-basic"
+          label="Your Name"
+          value={name}
+        />
+        <TextField
+          onChange={handleChange}
+          name="email"
+          id="outlined-basic"
+          label="Your Email"
+          value={email}
+        />
+        <TextField
+          onChange={handleChange}
+          name="subject"
+          id="outlined-basic"
+          label="Subject"
+          value={subject}
+        />
+        <TextField
+          onChange={handleChange}
+          name="message"
+          id="outlined-basic"
+          label="Message"
+          multiline
+          rows={10}
+          value={message}
+        />
+        <Button
+          style={{ width: 200, margin: "0 auto" }}
+          variant="contained"
+          type="submit"
+        >
+          Send
+        </Button>
+      </>
+    </form>
   );
 };
 
