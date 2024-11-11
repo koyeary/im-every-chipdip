fc7d5dd1679dca44eb91122fdd1a4f1774a9d2c6
koyeary <katyeary@gmail.com>
2024-11-11 15:57:22 -0500

    add engines package.json

--- config/db.js
+++ config/db.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  console.log("MONGODB_URI: ", process.env.MONGODB_URI);
   try {
     await mongoose.connect(process.env.MONGODB_URI);
     console.log("MongoDB Connected...");
